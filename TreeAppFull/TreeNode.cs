de158ddccb8ce68ff5534669eafd3f019bb2a3c0
Lucian Filip <Lucian.Filip@ivanti.com>
2019-06-03 11:32:58 +0300

    Added .NET 4.5.1 project.

---
+++ TreeAppFull/TreeNode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TreeAppFull
+{
+    public sealed class TreeNode
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public List<TreeNode> Children { get; set; }
+    }
+}
---
+++ TreeAppFull/TreeWriters.cs
@@ -0,0 +1,70 @@
+﻿using System.Data.SqlClient;
+using System.Linq;
+
+namespace TreeAppFull
+{
+    public interface ITreeWriter
+    {
+        void WriteTree(TreeNode rootNode);
+    }
+
+    public class DatabaseTreeWriter : ITreeWriter
+    {
+        private readonly string _connectionString;
+
+        public DatabaseTreeWriter(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void WriteTree(TreeNode rootNode)
+        {
+            CleanupTables();
+
+            WriteNode(rootNode, null);
+        }
+
+        private void WriteNode(TreeNode node, int? parentId)
+        {
+            WriteNodeInfo(node);
+            if (parentId != null)
+            {
+                WriteNodesRelation(node.Id, parentId.Value);
+            }
+
+            foreach (var child in node.Children ?? Enumerable.Empty<TreeNode>())
+            {
+                WriteNode(child, node.Id);
+            }
+        }
+
+        private void WriteNodeInfo(TreeNode node)
+        {
+            var sqlConn = new SqlConnection(_connectionString);
+            var sqlComm = new SqlCommand($"insert into TreeNodes (Id, Description) values (@id, @description)", sqlConn);
+            sqlComm.Parameters.AddWithValue("@id", node.Id);
+            sqlComm.Parameters.AddWithValue("@description", node.Description);
+            sqlComm.ExecuteNonQuery();
+        }
+
+        private void WriteNodesRelation(int nodeId, int parentId)
+        {
+            var sqlConn = new SqlConnection(_connectionString);
+            var sqlComm = new SqlCommand($"insert into TreeNodeRelations (Id, ParentId) values (@id, @parentId)", sqlConn);
+            sqlComm.Parameters.AddWithValue("@id", nodeId);
+            sqlComm.Parameters.AddWithValue("@parentId", parentId);
+            sqlComm.ExecuteNonQuery();
+        }
+
+        private void CleanupTables()
+        {
+            var sqlConn = new SqlConnection(_connectionString);
+            var sqlComm = new SqlCommand("", sqlConn);
+            sqlComm.CommandText = "delete from TreeNodes";
+            sqlComm.ExecuteNonQuery();
+
+            sqlComm.CommandText = "delete from TreeNodeRelations";
+            sqlComm.ExecuteNonQuery();
+        }
+    }
+}
